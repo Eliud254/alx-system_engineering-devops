b8941effd0e57930521f95041bd8adbedb9dfe28
Eliud254 <eliudobure@gmail.com>
2024-01-19 06:31:45 -0800

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Installs puppet-lint package
-
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+# -install puppet-lint
+package{'puppet-lint':
+ensure    => '2.5.0',
+provider  => 'gem'  
 }
