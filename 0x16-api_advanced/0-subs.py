37bc7aeebcdafbb99299d8b553305ba08677d96c
Eliud254 <eliudobure@gmail.com>
2024-03-05 14:19:28 -0800

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""
-Number of subscribers in  subreddit
-"""
-
-from requests import get
+"""Function to the query subscribers on the given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries all the Reddit API and returns the number 
-    of its subscribers.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "eliudobure"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""Function that  prints hot posts on the given Reddit subreddit."""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print all the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-            "User-Agent": "faithokoth"
-            }
-    params = {
-            "limit": 10
-            }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """
+    Prints the titles of the top 10 hottest posts on a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {"User-Agent": "eliudobure"}
+    params = {"limit": 10}
+
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+
+        results = response.json().get("data")
+        for post in results.get("children"):
+            print(post.get("data").get("title"))
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
